5ed7987258bf6e80df1ea503c4b65714ec400807
Henry Owusu Adjei <hadjei@uta.cv>
2026-07-04 12:52:03 +0200

    Time varying LCS and particle dispersal in CV

--- codes/010_run_experiments.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for day in {12..22}; do
-    echo $day
-    papermill 010_lcs_map.ipynb 010_lcs_map_2025-08-${day}.ipynb -p reference_time "2025-08-${day}" -k python &
-    sleep 1
-done
-
-wait
